2133cffd0ca5b622c13d81b60fa83dfc2ff63779
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2021-03-10 15:15:37 -0500

    Initial merge

---
+++ SortAlgorithm/SortAlgorithmTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SortAlgorithmTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SortAlgorithmTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c74981a5-7d41-4609-8be8-eefb0219ae5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SortAlgorithm/SortAlgorithmTest/SortAlgorithms.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SortAlgorithm.Model;
+
+namespace SortAlgorithmTest
+{
+    [TestClass]
+    public class SortAlgorithms
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
