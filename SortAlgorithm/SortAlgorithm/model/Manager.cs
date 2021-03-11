c8274ce8052012c23b6bcece8ff9fc05bcb1bad3
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-03-11 17:18:05 -0500

    Bubble sort experiment method created

--- SortAlgorithm/SortAlgorithm/model/Manager.cs
+++ SortAlgorithm/SortAlgorithm/model/Manager.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
+
 namespace SortAlgorithm.Model
 {
     public class Manager
     {
         private Dictionary<int, int> lenghts;
         private Algorithms st;
+
         public Manager()
         {
             st = new Algorithms();
@@ -20,9 +19,9 @@ namespace SortAlgorithm.Model
                 {3, 10000}
             };
         }
+
         public void MergeSortExperiment()
         {
- 
             List<String> data = new List<string>();
 
             int language = 2;
@@ -44,9 +43,41 @@ namespace SortAlgorithm.Model
                         sw.Stop();
                         var timeElapsed = timeTwo - timeOne;
 
-                        string info = algorithm+","+ language + ","+ j + "," + k;
+                        string info = algorithm + "," + language + "," + j + "," + k;
                         data.Add(info);
+                    }
+                }
+            }
 
+            WriteData(data);
+        }
+
+        public void BubbleSortExperiment()
+        {
+            List<String> data = new List<string>();
+
+            int language = 2;
+            int algorithm = 1;
+
+            for (int j = 1; j < 4; j++)// i is the array Size (10^2, 10^3, 10^4)
+            {
+                for (int k = 1; k < 4; k++) //k is the array State(random,descendent,ascendent)
+                {
+                    int[] array = GenerateArray(j, k);
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Stopwatch sw = new Stopwatch();
+                        sw.Start();
+                        st.BubbleSort(array);
+                        sw.Stop();
+                        TimeSpan ts = sw.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+
+                        string info = algorithm + "," + language + "," + j + "," + k + "," + elapsedTime;
+                        data.Add(info);
                     }
                 }
             }
@@ -63,21 +94,19 @@ namespace SortAlgorithm.Model
             int max = int.MaxValue;
             int min = 0;
 
-
             for (int i = 0; i < size; i++)
             {
                 int number = random.Next(min, max);
                 array[i] = number;
 
-                if (status==2)//orden descendiente
+                if (status == 2)//orden descendiente
                 {
                     max = (number + max) / 2;
                 }
-                else if(status==3)// orden ascendiente
+                else if (status == 3)// orden ascendiente
                 {
-                    min = (min+number)/2;
+                    min = (min + number) / 2;
                 }
-
             }
 
             return array;
@@ -85,7 +114,6 @@ namespace SortAlgorithm.Model
 
         public void WriteData(List<string> data)
         {
-
         }
     }
 }
